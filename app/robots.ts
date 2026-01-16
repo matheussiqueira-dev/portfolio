8fbecd55a0d224f56a7c720eb65d85a162141142
Matheus Siqueira <matheussiqueirahub@gmail.com>
2026-01-16 16:14:25 -0300

    chore: seo, sitemap, robots and assets

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
+    host: "https://www.matheussiqueira.dev",
     sitemap: "https://www.matheussiqueira.dev/sitemap.xml",
   };
 }
