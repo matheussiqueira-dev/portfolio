90abd831010a7aee2925d536a4cd445bcd920f90
Matheus Siqueira <matheussiqueirahub@gmail.com>
2026-01-22 13:25:42 -0300

    feat: add Google Tag Manager (GTM-WR93ZJ8C)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import Header from "@/components/layout/Header";
 import JsonLd from "@/components/seo/JsonLd";
 import { baseUrl, siteName, sameAsLinks } from "@/lib/seo";
 import "./globals.css";
+
+// Google Tag Manager ID
+const GTM_ID = "GTM-WR93ZJ8C";
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -133,9 +137,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google Tag Manager - Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-black text-white`}
       >
+        {/* Google Tag Manager - NoScript fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <JsonLd data={[websiteJsonLd, personJsonLd]} />
         <Header />
         <main className="min-h-screen">{children}</main>
