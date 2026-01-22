62b58f9608a80fb831fafe05ebfaff8c2ff926fb
Matheus Siqueira <matheussiqueirahub@gmail.com>
2026-01-22 12:47:36 -0300

    fix: remove duplicate 'use client' directive in LanguageSwitch

--- components/ui/LanguageSwitch.tsx
+++ components/ui/LanguageSwitch.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-"use client";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function LanguageSwitch() {
   const pathname = usePathname();
-
   const isEnglish = pathname.startsWith("/en");
 
   return (
